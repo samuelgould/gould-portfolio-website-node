386a30ad0cd50f1201204861a67e9bac579d781c
Sam Gould <sjg326@gmail.com>
2018-04-20 11:38:40 -0700

    completed persistent navigation for sticky hamburger icon in desktop view

--- public/index.js
+++ public/index.js
@@ -2,5 +2,6 @@
 
 const toggleHamburger = () => {
   document.getElementById('hamburger-icon').classList.toggle('change');
+  document.getElementById('desktop-hamburger-icon').classList.toggle('change');
   document.getElementById('nav-overlay').classList.toggle('show-overlay');
 };
